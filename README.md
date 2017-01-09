2c0450bfedd02fca9a724202c76e550918bbfdff
Dan Steinman <dansteinman@gmail.com>
2017-01-09 09:39:30 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# ghoulies
-a headless client integration test runner
+`ghoulies` is a headless integration test runner, built on top `mocha` which can run your server and client tests in the same script.
+
+## Installation
+---
+
+Prerequisites: ghoulies is intended to be used along with mocha/chai:
+
+```
+npm install mocha --save
+npm install chai --save
+```
+
+Install:
+
+```
+npm install ghoulies --save
+npm install ghoulie --save
+```
+
+## Usage
+---
+
+Add ghoulies to your mocha/chai test:
+
+```
+var chai = require('chai');
+var expect = chai.expect;
+
+// include ghoulies
+var ghoulies = require('ghoulies');
+
+// url to local development server
+var url = "http://localhost:1337";
+
+before(function (done) {	
+	 ghoulies.client({
+		url: url,
+		globals: true,
+		jquery: true
+	}, function (window, ghoulie) {
+		done();
+	});
+});
+
+describe('ghoulies', () => {
+	it('loads my application', (done) => {
+	
+		// ghoulies exposes a headless browser window object
+		expect(typeof window).to.be.equal('object');
+		
+		// use jQuery to inspect the DOM
+		var myAppNode = $('#myapp');
+		expect(myAppNode.length).to.be.equal(1);
+		
+		// initialize ghoulie at the end of the first test
+		ghoulie.init();
+		
+		done();
+	});
+});
+
+```
+
+Add ghoulies `npm run` commands to your package.json:
+
+
+
+## Run The Ghoulie Tests
+---
+Run all your ghoulie tests:
+```
+npm run ghoulies
+```
+
+Run all your ghoulie tests re-run when changes occur to the tests:
+```
+npm run ghoulies:watch
+```
+
+## Examples
+---
+
+The best example to date of a ghoulie test is the one supplied in the `sails-react-redux-boilerplate` example application.
+
+See:
+<a href="https://github.com/jaxcore/sails-react-redux-boilerplate/blob/master/test/ghoulies/todos.test.js">https://github.com/jaxcore/sails-react-redux-boilerplate/blob/master/test/ghoulies/todos.test.js</a>
---
+++ index.js
@@ -0,0 +1,94 @@
+
+var jsdom = require('jsdom');
+var chalk = require('chalk');
+var EventEmitter = require('events').EventEmitter;
+
+function Ghoulies() {
+	this.constructor();
+	this.version = '0.0.1';
+	this.log({ghoulieName:'ghoulies '+this.version},'(c) 2016 Jaxcore', 'MIT Licensed');
+}
+Ghoulies.prototype = Object.create(EventEmitter.prototype);
+Ghoulies.prototype.constructor = EventEmitter;
+Ghoulies.prototype.log = function() {
+	var args = Array.prototype.slice.call(arguments);
+	args = args.map(function (arg,index) {
+		if (index===0 && typeof arg==='object' && arg.ghoulieName) {
+			return chalk.green(arg.ghoulieName);
+		}
+		if (typeof arg === 'string') {
+			//if (index>=2) return arg;
+			if (index>=2 && typeof args[0]==='object' && args[0].ghoulieName && typeof args[1]==='string') return arg;
+			return chalk.cyan(arg);
+		}
+		if (typeof arg === 'number') return chalk.magenta(arg);
+		if (arg===null) return chalk.yellow('null');
+		if (typeof arg === 'undefined') return chalk.yellow('undefined');
+		return arg;
+	});
+	console.log.apply(null, args);
+};
+Ghoulies.prototype.client = function(options, callback) {
+	// if (typeof global.before!=='function') {
+	// 	throw new Error('Ghoulies error: before is not a function');
+	// }
+	// if (typeof global.before==='function') {
+		if (typeof options==='string') {
+			var url = options;
+			options = {
+				url: url,
+				globals: true,
+				jquery: true
+			}
+		}
+		var me = this;
+		this.name = options.name || 'ghoulie';
+	
+		//callback(function (done) {
+			me.jsdom(options, function (window, ghoulie) {
+				me.log({ghoulieName:me.name}, 'client loaded', options);
+				if (typeof callback === 'function') {
+					callback(window, ghoulie);
+				}
+				else callback();
+			});
+		//});
+	//}
+};
+
+Ghoulies.prototype.jsdom = function (options, callback) {
+	var scripts = options.scripts || [];
+	if (options.jquery) scripts.push('http://code.jquery.com/jquery.js');
+	
+	var me = this;
+	jsdom.env({
+		url: options.url,
+		scripts: scripts,
+		done: function(err, currWindow) {
+			if (!currWindow) {
+				throw new Error('Ghoulies error: no window');
+			}
+			if (options.globals) {
+				if (typeof window === 'undefined') global.window = currWindow;
+				else throw new Error('Ghoulies: global.window already defined');
+				if (typeof document === 'undefined') global.document = currWindow.document;
+				else throw new Error('Ghoulies: global.document already defined');
+				if (typeof $ === 'undefined') global.$ = currWindow.$;
+				else throw new Error('Ghoulies: global.$ already defined');
+				if (typeof ghoulie === 'undefined') global.ghoulie = currWindow.__ghoulie;
+				else throw new Error('Ghoulies: global.ghoulie already defined');
+			}
+			currWindow.__ghoulie.ghoulies = me;
+			callback(currWindow, currWindow.__ghoulie);
+		},
+		features: {
+			FetchExternalResources: ["script"],
+			ProcessExternalResources: ["script"],
+			SkipExternalResources: false
+		}
+	});
+};
+
+var ghoulies = new Ghoulies();
+global.__ghoulies = ghoulies;
+module.exports = ghoulies;
